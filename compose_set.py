f1a5cc003b13e4f87069ca765690c46416a0dab5
mtb_beta <mtb.toya0403@gmail.com>
2018-03-14 00:30:11 +0900

    曲が作れること

--- compose_set.py
+++ compose_set.py
@@ -1,1 +1,2 @@
-
+def create_song():
+    return True
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,17 @@
 import unittest
 
 class TestSongCompose(unittest.TestCase):
-    def test_1st_step(self):
+    def test_compose_import(self):
+        """
+        パッケージがimportできること
+        """
         import compose_set
         self.assertTrue(compose_set)
+
+    def test_create_song(self):
+        """
+        曲を作れること
+        """
+        import compose_set
+        song = compose_set.create_song()
+        self.assertTrue(song)
