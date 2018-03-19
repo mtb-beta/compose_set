feb656c6b9d04279122d0ca593f655e43339c169
mtb_beta <mtb.toya0403@gmail.com>
2018-03-19 22:10:15 +0900

    あるキーのメジャースケールで3の音がわかる close #35

--- tests/test_milestone2.py
+++ tests/test_milestone2.py
@@ -5,7 +5,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_C_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー C の場合
+        キー C の場合
         """
         import compose.core
         scale = compose.core.Scale(key="C")
@@ -15,7 +15,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_D_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー D の場合
+        キー D の場合
         """
         import compose.core
         scale = compose.core.Scale(key="D")
@@ -25,7 +25,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_E_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー E の場合
+        キー E の場合
         """
         import compose.core
         scale = compose.core.Scale(key="E")
@@ -35,7 +35,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_F_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー F の場合
+        キー F の場合
         """
         import compose.core
         scale = compose.core.Scale(key="F")
@@ -45,7 +45,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_G_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー G の場合
+        キー G の場合
         """
         import compose.core
         scale = compose.core.Scale(key="G")
@@ -55,7 +55,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_A_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー A の場合
+        キー A の場合
         """
         import compose.core
         scale = compose.core.Scale(key="A")
@@ -65,7 +65,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_1t_key_in_B_major_scale(self):
         """
         あるキーのメジャースケールで１の音がわかる
-        デフォルトキー B の場合
+        キー B の場合
         """
         import compose.core
         scale = compose.core.Scale(key="B")
@@ -75,7 +75,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_C_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー C の場合
+        キー C の場合
         """
         import compose.core
         scale = compose.core.Scale(key="C")
@@ -85,7 +85,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_D_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー D の場合
+        キー D の場合
         """
         import compose.core
         scale = compose.core.Scale(key="D")
@@ -95,7 +95,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_E_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー E の場合
+        キー E の場合
         """
         import compose.core
         scale = compose.core.Scale(key="E")
@@ -105,7 +105,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_F_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー F の場合
+        キー F の場合
         """
         import compose.core
         scale = compose.core.Scale(key="F")
@@ -115,7 +115,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_G_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー G の場合
+        キー G の場合
         """
         import compose.core
         scale = compose.core.Scale(key="G")
@@ -125,7 +125,7 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_A_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー A の場合
+        キー A の場合
         """
         import compose.core
         scale = compose.core.Scale(key="A")
@@ -135,9 +135,79 @@ class TestMileStone2(unittest.TestCase):
     def test_scale_2nd_key_in_B_major_scale(self):
         """
         あるキーのメジャースケールで2の音がわかる
-        デフォルトキー B の場合
+        キー B の場合
         """
         import compose.core
         scale = compose.core.Scale(key="B")
         note = scale.note(2)
         self.assertEqual(note.name, 'C_')
+
+    def test_scale_3rd_key_in_C_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー C の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="C")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'E')
+
+    def test_scale_3rd_key_in_D_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー D の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="D")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'F_')
+
+    def test_scale_3rd_key_in_E_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー E の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="E")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'G_')
+
+    def test_scale_3rd_key_in_F_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー F の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="F")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'A')
+
+    def test_scale_3rd_key_in_G_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー G の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="G")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'B')
+
+    def test_scale_3rd_key_in_A_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー A の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="A")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'C_')
+
+    def test_scale_3rd_key_in_B_major_scale(self):
+        """
+        あるキーのメジャースケールで3の音がわかる
+        キー B の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="B")
+        note = scale.note(3)
+        self.assertEqual(note.name, 'D_')
