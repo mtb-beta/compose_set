4d075420353b741387aaec6b6611d384e08a292b
mtb_beta <mtb.toya0403@gmail.com>
2018-03-19 21:58:35 +0900

    あるキーのメジャースケールで2の音がわかる close #34

--- compose/core/__init__.py
+++ compose/core/__init__.py
@@ -6,24 +6,40 @@ class ChromaticScale(enum.Enum):
     クロマチックスケール
     # は _ で表示している。
     """
-    C = 1
-    C_ = 2
-    D = 3
-    D_ = 4
-    E = 5
-    F = 6
-    F_ = 7
-    G = 8
-    G_ = 9
-    A = 10
-    A_ = 11
-    B = 12
+    C = 0
+    C_ = 1
+    D = 2
+    D_ = 3
+    E = 4
+    F = 5
+    F_ = 6
+    G = 7
+    G_ = 8
+    A = 9
+    A_ = 10
+    B = 11
+
+# メジャースケール
+MajarScale = {
+    1 : 0,
+    2 : 2,
+    3 : 4,
+    4 : 5,
+    5 : 7,
+    6 : 9,
+    7 : 11,
+}
+
 
 class Scale:
-    def __init__(self, key="C"):
+    def __init__(self, key="C", scale_type=MajarScale):
         self.key = key
+        self.scale_type = scale_type
 
     def note(self, number):
-        self.offset = ChromaticScale[self.key].value - 1
-        return ChromaticScale(number + self.offset)
+        # キーがずれている数だけオフセットを設定する
+        self.offset = ChromaticScale[self.key].value
+        # numberで指定された度数だけずらす。同じノートが1度なので-1する
+        note = (self.scale_type[number] + self.offset)%12
+        return ChromaticScale(note)
 
--- tests/test_milestone2.py
+++ tests/test_milestone2.py
@@ -71,3 +71,73 @@ class TestMileStone2(unittest.TestCase):
         scale = compose.core.Scale(key="B")
         note = scale.note(1)
         self.assertEqual(note.name, 'B')
+
+    def test_scale_2nd_key_in_C_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー C の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="C")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'D')
+
+    def test_scale_2nd_key_in_D_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー D の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="D")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'E')
+
+    def test_scale_2nd_key_in_E_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー E の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="E")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'F_')
+
+    def test_scale_2nd_key_in_F_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー F の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="F")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'G')
+
+    def test_scale_2nd_key_in_G_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー G の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="G")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'A')
+
+    def test_scale_2nd_key_in_A_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー A の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="A")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'B')
+
+    def test_scale_2nd_key_in_B_major_scale(self):
+        """
+        あるキーのメジャースケールで2の音がわかる
+        デフォルトキー B の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="B")
+        note = scale.note(2)
+        self.assertEqual(note.name, 'C_')
