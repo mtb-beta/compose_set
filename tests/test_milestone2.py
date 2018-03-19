3a989c4dee2c5c2105e28174e7d561e67a5bb564
mtb_beta <mtb.toya0403@gmail.com>
2018-03-19 22:31:11 +0900

    あるキーのメジャースケールで5の音がわかる close #37 #38 #39

--- tests/test_milestone2.py
+++ tests/test_milestone2.py
@@ -281,3 +281,73 @@ class TestMileStone2(unittest.TestCase):
         scale = compose.core.Scale(key="B")
         note = scale.note(4)
         self.assertEqual(note.name, 'E')
+
+    def test_scale_5th_key_in_C_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー C の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="C")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'G')
+
+    def test_scale_5th_key_in_D_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー D の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="D")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'A')
+
+    def test_scale_5th_key_in_E_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー E の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="E")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'B')
+
+    def test_scale_5th_key_in_F_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー F の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="F")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'C')
+
+    def test_scale_5th_key_in_G_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー G の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="G")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'D')
+
+    def test_scale_5th_key_in_A_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー A の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="A")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'E')
+
+    def test_scale_5th_key_in_B_major_scale(self):
+        """
+        あるキーのメジャースケールで5の音がわかる
+        キー B の場合
+        """
+        import compose.core
+        scale = compose.core.Scale(key="B")
+        note = scale.note(5)
+        self.assertEqual(note.name, 'F_')
